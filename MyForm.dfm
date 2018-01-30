e85487ffc2ac64b65b681150d5e580bdd6d6bf15
mroman48389 <mroman@avenirhealth.org>
2018-01-30 12:43:30 -0500

    Added a label and button.

--- MyForm.dfm
+++ MyForm.dfm
@@ -11,6 +11,23 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 24
+    Top = 24
+    Width = 31
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Button1: TButton
+    Left = 24
+    Top = 64
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
 end
--- MyForm.pas
+++ MyForm.pas
@@ -4,10 +4,14 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +24,16 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  label1.caption := 'I just clicked a button.';
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  label1.caption := '';
+  button1.caption := 'Click me please';
+end;
 
 end.
